c8f1faa901a0325e82074cb2b7b44dd2b142f22a
AdrianKs <adrian.kochsiek@outlook.de>
2023-09-06 09:10:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# A Benchmark for Semi-Inductive Link Prediction in Knowledge Graphs
+
+This is the benchmark, code, and configuration accompanying the paper [A Benchmark for Semi-Inductive Link Prediction in Knowledge Graphs]().
+The main branch holds code/information about the benchmark itself. 
+The following branches hold code and configuration for the separate models evaluated in the study.
+
+- [KGT5 \& KGT5-context]()
+- [ComplEx + Bias + FoldIn \& DisMult ERAvg]()
+- [HittER]()
+
+
+## Benchmark
+
+### Download data
+
+```
+mkdir data
+cd data
+curl -O https://web.informatik.uni-mannheim.de/pi1/kge-datasets/wikidata5m_v3_semi_inductive.tar.gz
+tar -zxvf wikidata5m_v3_semi_inductive.tar.gz
+```
+
+### Generate Few Shot Tasks
+
+- use the file `prepare_few_shot.py`
+- create a `few_shot_set_creator` object
+	- `dataset_name`: (str) name of the dataset
+      - default: wikidata5m_v3_semi_inductive
+	- `use_invese`: (bool) whether to use inverse relations
+      - default: False
+      - if True: for all triples where the unseen entity is in the object slot, increase relation id by num-relations and invert triple
+	- `split`: (str) which split to use
+      - default: valid
+	- `context_selection`: (str) which context\_selection technique to use
+      - default: most\_common
+      - options: most\_common, least\_common, random
+
+```
+few_shot_set_creator = FewShotSetCreator(
+	dataset_name="wikidata5m_v3_semi_inductive",
+	use_inverse=True,
+	split="test"
+)
+```
+
+- generate the data using the `few_shot_set_creator`
+	- `num_shots`: (int) the number of shots to use (between 0 and 10)
+
+```
+data = few_shot_set_creator.create_few_shot_dataset(num_shots=5)
+```
+
+- evaluation is performed in direction unseen to seen
+- output format looks like this
+```
+[
+{
+	"unseen_entity": <id of unseen entity>,
+	"unseen_slot": <slot of unseen entity: 0 for head/subject, 2 for tail/object>,
+	"triple: <[s, p, o]>,
+	"context: <[unseen_entity_id, unseen_entity_slot, s, p, o]>
+},
+...
+
+]
+
---
+++ prepare_few_shot.py
@@ -0,0 +1,98 @@
+import os
+import torch
+import argparse
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+
+class FewShotSetCreator:
+    def __init__(
+            self,
+            dataset_name="wikidata5m_v3_semi_inductive",
+            split="valid",
+            use_inverse=False,
+            context_selection="most_common",
+    ):
+        self.dataset_name = dataset_name
+        self.split = split
+        self.use_inverse = use_inverse
+        self.context_selection=context_selection
+        if self.use_inverse:
+            train_triples = self._load_train_triples()
+            self.num_relations = len(np.unique(train_triples[:, 1]))
+        self.triple_pool = self._load_triple_pool()
+
+    def _load_train_triples(self):
+        triples = pd.read_csv(
+            os.path.join("data", self.dataset_name, "train.del"),
+            delimiter="\t",
+            header=None
+        ).to_numpy()
+        return triples
+
+    def _load_triple_pool(self):
+        triple_pool = pd.read_csv(
+            os.path.join("data", self.dataset_name, f"{self.split}_pool.del"),
+            delimiter="\t",
+            header=None
+        ).to_numpy()
+        if self.use_inverse:
+            triple_inverse_pool = np.copy(triple_pool)
+            triple_inverse_pool[:, 3] += self.num_relations
+            triple_inverse_pool[:, 2] = np.copy(triple_pool[:, 4])
+            triple_inverse_pool[:, 4] = np.copy(triple_pool[:, 2])
+
+            relevant_triple_pool = triple_pool[triple_pool[:, 1] == 0]
+            relevant_inverse_triple_pool = triple_inverse_pool[triple_inverse_pool[:, 1] == 2]
+            relevant_inverse_triple_pool[:, 1] = 0
+            triple_pool = np.concatenate((relevant_triple_pool, relevant_inverse_triple_pool))
+
+        return triple_pool
+
+    def create_few_shot_dataset(self, num_shots):
+        print(f"create few shot set for {self.split} with {num_shots} shots")
+        # convert to torch as split behaves differently in torch compared to numpy
+        triples_per_entity = [t.numpy() for t in torch.from_numpy(self.triple_pool).split(11)]
+        eval_list = list()
+        for tpe in tqdm(triples_per_entity):
+            for i in range(len(tpe)):
+                mask = np.ones((len(tpe,)), dtype=np.bool)
+                mask[i] = False
+                triple = tpe[i]
+                context = tpe[mask]
+                if self.context_selection == "most_common":
+                    context = context[:num_shots]
+                elif self.context_selection == "least_common":
+                    context = context[-num_shots:]
+                elif self.context_selection == "random":
+                    # numpy shuffles in place
+                    np.random.shuffle(context)
+                    context = context[:num_shots]
+                eval_dict = {
+                    "unseen_entity": triple[0].item(),
+                    "unseen_slot": triple[1].item(),
+                    "triple": triple[2:],
+                    "context": context
+                }
+                eval_list.append(eval_dict)
+        return eval_list
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", "-d", type=str, default="wikidata5m_v3_semi_inductive")
+    parser.add_argument("--split", "-s", type=str, default="valid")
+    parser.add_argument("--num_shots", "-k", type=int, default=10)
+    args = parser.parse_args()
+
+    few_shot_set_creator = FewShotSetCreator(
+        dataset_name=args.dataset,
+        split=args.split,
+    )
+
+    few_shot_set_creator.create_few_shot_dataset(args.num_shots)
+
+
+
+
